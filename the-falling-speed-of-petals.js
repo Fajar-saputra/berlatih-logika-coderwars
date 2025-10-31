659509927799d1c95a42440bd216d56d6e2b46d4
Fajar Saputra <fajarsaputra2305@gmail.com>
2025-10-31 11:58:48 +0700

    feat: Solve 'The falling speed of petals' challenge

---
+++ the-falling-speed-of-petals.js
@@ -0,0 +1,11 @@
+function sakuraFall(v) {
+    const distance = 400;
+
+    if (v === 0) {
+    }
+    const time = distance / v;
+    return time.toFixed(3);
+}
+
+// tinggi = kecepatan * waktu
+// 400 = 5 * 80
